85097aaf56afa6f05f71d2ef12e471de8d58319d
bastiivc <basti.mejiasss@gmail.com>
2025-10-20 00:15:12 -0300

    cambio db local a nube

--- backend_rw/src/database.ts
+++ backend_rw/src/database.ts
@@ -1,22 +1,27 @@
 import { Sequelize } from 'sequelize';
 
-// Reemplaza con tus propias credenciales
-const sequelize = new Sequelize('racing_db', 'postgres', 'c10h15n1', {
-  host: 'localhost',
+const dbUrl = 'postgresql://neondb_owner:npg_JYNG38cthdXC@ep-shiny-breeze-acdy3wk7-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
+
+const sequelize = new Sequelize(dbUrl, {
   dialect: 'postgres',
-  logging: false
+  protocol: 'postgres',
+  dialectOptions: { // Necesario para conexiones seguras (SSL) en la nube
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
 });
 
-// Probar la conexión
 async function testConnection() {
   try {
     await sequelize.authenticate();
-    console.log('Conexión a la base de datos establecida correctamente.');
+    console.log('✅ Conexión a la base de datos de Supabase establecida correctamente.');
   } catch (error) {
-    console.error('No se pudo conectar a la base de datos:', error);
+    console.error('❌ No se pudo conectar a la base de datos de Supabase:', error);
   }
 }
 
 testConnection();
 
-export default sequelize; // Usamos export default
+export default sequelize;
